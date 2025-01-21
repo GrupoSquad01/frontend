7f3c06bd1b14928e5308d4f523fdb32bb87f43fe
Leticia Baungardt <leticiabaungardt@gmail.com>
2025-01-20 22:36:14 -0300

    Finalizado a parte web

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -3,9 +3,15 @@ import FotoPerfil from "../assets/Foto perfil.png";
 import AvatarF1 from "../assets/F1.png";
 import AvatarF2 from "../assets/F2.png";
 import AvatarF3 from "../assets/F3.png";
-import AvatarM1 from "../assets/F3.png";
-import AvatarM2 from "../assets/F3.png";
-import AvatarM3 from "../assets/F3.png";
+import AvatarM1 from "../assets/M1.jpg";
+import AvatarM2 from "../assets/M2.jpg";
+import AvatarM3 from "../assets/M3.jpg";
+import { MdPerson } from "react-icons/md";
+import { MdPhoneAndroid } from "react-icons/md";
+import { MdPersonPin } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+import { MdOutlinePassword } from "react-icons/md";
+import { TbPointFilled } from "react-icons/tb";
 
 export default function Perfil() {
     return(
@@ -55,28 +61,43 @@ export default function Perfil() {
                         <div className="">
                            <div className="grid">
                                 <label htmlFor="" for="name" className="titulo_input">Nome completo</label>
-                                <input type="text" name="name" id="name" required placeholder="João Silva" className="input"/>
+                                <div className="div_input">
+                                    <MdPerson className="icon_input"/>
+                                    <input type="text" name="name" id="name" required placeholder="João Silva" className="input"/>
+                                </div>
                             </div>
 
                             <div className="grid">
                                 <label htmlFor="" for="telephone" className="titulo_input">Telefone</label>
-                                <input type="tel" id="telephone" name="telephone" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" required placeholder="(xx) xxxxx-xxxx" className="input"/>
+                                <div className="div_input">
+                                  <MdPhoneAndroid className="icon_input"/>
+                                  <input type="tel" id="telephone" name="telephone" pattern="[0-9]{2}-[0-9]{5}-[0-9]{4}" required placeholder="(xx) xxxxx-xxxx" className="input"/>  
+                                </div>
                             </div> 
                         </div>
 
                         <div className="grid">
                             <label htmlFor="" for="username" className="titulo_input">Username</label>
-                            <input type="text" name="username" id="username" required placeholder="joazinho" className="input"/>
+                            <div className="div_input">
+                               <MdPersonPin className="icon_input"/>
+                                <input type="text" name="username" id="username" required placeholder="joazinho" className="input"/>
+                            </div>
                         </div>
                         
                         <div className="grid">
                             <label htmlFor="" for="email" className="titulo_input">E-mail</label>
-                            <input type="email" name="email" id="email" required placeholder="username@gmail.com" className="input"/>
+                            <div className="div_input">
+                                <MdEmail className="icon_input"/>
+                                <input type="email" name="email" id="email" required placeholder="username@gmail.com" className="input"/>
+                            </div>
                         </div>
 
                         <div className="grid">
                             <label htmlFor="" for="password" className="titulo_input">Senha</label>
-                            <input type="password" name="password" id="password" required className="input"/>
+                            <div className="div_input">
+                                <MdOutlinePassword className="icon_input"/>
+                                <input type="password" name="password" id="password" required placeholder="••••••••" className="input"/>
+                            </div>
                         </div>
 
                         <div className="flex justify-end gap-3 py-6">
